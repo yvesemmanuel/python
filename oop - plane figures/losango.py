771b0ccc8ea90c41848ae91a613bd066e40d9566
Yves Emmanuel <83613347+yvesemmanuel@users.noreply.github.com>
2022-03-15 23:13:51 -0300

    deleting all files with a colon in the name

--- oop - plane figures/losango.py
+++
@@ -1,9 +0,0 @@
-import turtle
-
-class Losango:
-    def __init__(self, diagonal1, diagonal2):
-        self.diagonal1 = diagonal1
-        self.diagonal2 = diagonal2
-
-    def get_area(self):
-        return self.diagonal1 * self.diagonal2 / 2
